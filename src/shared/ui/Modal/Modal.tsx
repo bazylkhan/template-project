5d77ab0e099d8429bc5b03619cf22b9208a2b337
bazyl <bazylkhan.personal@gmail.com>
2026-08-15 00:54:12 +0500

    add modal, portal

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,20 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+export const Modal = (props:ModalProps) => {
+    const { className, children } = props;
+    return (
+        <div className={classNames(cls.Modal, {}, [className])}>
+            <div className={cls.overlay}>
+                <div className={cls.content}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
